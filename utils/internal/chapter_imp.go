a7b8f2acd82932f3db63393fda1762117e2a0011
郭浩 <korman@qq.com>
2020-02-16 01:15:37 +0800

    Added page

--- utils/internal/chapter_imp.go
+++ utils/internal/chapter_imp.go
@@ -2,6 +2,8 @@ package internal
 
 import (
 	"comic_server/pb"
+	"io/ioutil"
+	"log"
 )
 
 type ChapterImp struct {
@@ -14,6 +16,40 @@ type ChapterImp struct {
 func (ptr *ChapterImp) Init() {
 	ptr.info = &pb.PbChapterInfo{}
 	ptr.info.Name = ptr.FileName
+
+	readerInfo, err := ioutil.ReadDir(ptr.FilePath)
+
+	if err != nil {
+		log.Fatal(err.Error())
+		return
+	}
+
+	if nil == ptr.pageList {
+		ptr.pageList = &pb.PbPageList{}
+	}
+
+	var pages []*pb.PbPageInfo = make([]*pb.PbPageInfo, 0)
+
+	for _, info := range readerInfo {
+		if info == nil {
+			continue
+		}
+
+		if info.IsDir() {
+			continue
+		}
+
+		page := &PageImp{
+			FileName: info.Name(),
+			FilePath: ptr.FilePath + "/" + info.Name(),
+		}
+
+		page.Init()
+
+		pages = append(pages, page.GetPageInfo())
+	}
+
+	ptr.pageList.Pages = pages
 }
 
 func (ptr *ChapterImp) GetChapterInfo() (p *pb.PbChapterInfo) {
